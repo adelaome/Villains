2ee3b940021c0b4ea6917a635b38726ffd31ace3
Anibal De La O <adelaome@uncc.edu>
2017-05-04 15:20:11 +0000

    Complete create and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    
     protect_from_forgery with: :exception
+    before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |user_params|
